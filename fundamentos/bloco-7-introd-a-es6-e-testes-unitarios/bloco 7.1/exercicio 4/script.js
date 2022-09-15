133f84e642258d381e87ed0b14c8b40e951b42d6
Otavio <suizld@pm.me>
2022-09-15 20:01:36 -0300

    Mudancas no codigo do exercicio 4 do dia 1 do bloco 7

--- fundamentos/bloco-7-introd-a-es6-e-testes-unitarios/bloco 7.1/exercicio 4/script.js
+++ fundamentos/bloco-7-introd-a-es6-e-testes-unitarios/bloco 7.1/exercicio 4/script.js
@@ -2,9 +2,7 @@
 */
 
 const substituaX = (nome) => {
-
   const frase = 'Tryber x aqui!';
-
   const fraseDividida = frase.split((' '));
   fraseDividida[1] = nome;
   const arrayJuntado = fraseDividida.join(' ');
@@ -12,11 +10,8 @@ const substituaX = (nome) => {
 }
 
 const minhasSkills = (tryber) => {
-  
   const skills = ['HTML', 'CSS', 'JavaScript'];
-
   let concatenedString = `${tryber} Minhas três principais habilidades são: ${skills}`
-
   return console.log(concatenedString);
 }
 
