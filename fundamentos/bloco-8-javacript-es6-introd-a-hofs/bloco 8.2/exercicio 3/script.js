1ab9383d0e23647e6ce5fc55bdf2754a60b4c25f
Otavio <suizld@pm.me>
2022-09-23 19:54:19 -0300

    Adicionado exercicio 3 do dia 2 do bloco 8

---
+++ fundamentos/bloco-8-javacript-es6-introd-a-hofs/bloco 8.2/exercicio 3/script.js
@@ -0,0 +1,18 @@
+/*
+*/
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+const hasName = (arr, name) => arr.some((element) => element === name);
+console.log(hasName(names, 'Ana'));
+
+///
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+const verifyAges = (arr, minimumAge) => arr.every((element) => element.age >= minimumAge);
+console.log(verifyAges(people, 18));
