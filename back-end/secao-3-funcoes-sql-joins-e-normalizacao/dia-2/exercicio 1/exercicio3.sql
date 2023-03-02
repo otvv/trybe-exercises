b405176a20f77c29f52f7789320ee33b59fa77f4
Otavio <suizld@pm.me>
2023-03-02 09:47:50 -0300

    Outra correcao...

--- back-end/secao-3-funcoes-sql-joins-e-normalizacao/dia-2/exercicio 1/exercicio3.sql
+++ back-end/secao-3-funcoes-sql-joins-e-normalizacao/dia-2/exercicio 1/exercicio3.sql
@@ -1,5 +1,5 @@
 -- EXERCICIO 3
-SELECT mo.title, bo.rating FROM movies AS mo
+SELECT mo.title, bo.rating FROM pixar.movies AS mo
 INNER JOIN pixar.box_office AS bo
 ON mo.id = bo.movie_id
 ORDER BY bo.rating DESC;
